81b5bc2004908cd5c5609d1d68791e186d0bc0aa
Steve Trewick <steve.trewick@icloud.com>
2025-09-26 13:16:24 +0100

    initial commit

---
+++ Wire.h
@@ -0,0 +1,41 @@
+#ifndef _WIRE_H_
+#define _WIRE_H_
+
+#include "hardware/i2c.h"
+#include "pico/stdlib.h"
+#include <cstdint>
+
+class WireClass {
+public:
+    WireClass(i2c_inst_t *i2cPort = i2c0, uint sdaPin = 21, uint sclPin = 22);
+
+    void begin(); // init with defaults
+    void beginTransmission(uint8_t address);
+    uint8_t endTransmission();
+
+    void write(uint8_t data);
+    void write(uint8_t *data, size_t length);
+
+    uint8_t requestFrom(uint8_t address, uint8_t quantity);
+    int available();
+    int read();
+
+private:
+    i2c_inst_t *i2cPort;
+    uint sdaPin;
+    uint sclPin;
+    uint8_t txAddress;
+    static const int MAX_BUFFER = 32;
+
+    uint8_t txBuffer[MAX_BUFFER];
+    int txIndex;
+    int txLength;
+
+    uint8_t rxBuffer[MAX_BUFFER];
+    int rxIndex;
+    int rxLength;
+};
+
+extern WireClass Wire;
+
+#endif
